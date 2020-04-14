dbb3a7354c3566b3c9b3c676b697ebfc4064f5c7
Jubenal <jubenalcastaneda@gmail.com>
2020-04-14 14:35:19 -0600

    Agregados elementos de boostrap haciendo funcional la parte del diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{RouterModule,Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortadaComponent } from './portada/portada.component';
 
+
+const routes:Routes = [{path:'',component:PortadaComponent}]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
